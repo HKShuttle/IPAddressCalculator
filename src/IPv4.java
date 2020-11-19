bd0ad941e8df28f2ab51b8310b744a5197c5fdde
HKShuttle <terakou1105@gmail.com>
2020-11-19 20:20:08 +0900

    create class IPv4Exec

--- src/IPv4.java
+++ src/IPv4.java
@@ -1,16 +1,36 @@
+import java.util.stream.Stream;
+
 public class IPv4 {
     private int address;
     private int prefix;
     private int netMask;
 
     public IPv4(int address, int prefix) {
-        this.address = address;
-        this.prefix = prefix;
+        setAddress(address);
+        setPrefix(prefix);
+        calculateNetMask();
+    }
+
+    public IPv4(String address, int prefix) {
+        setAddress(address);
+        setPrefix(prefix);
         calculateNetMask();
     }
 
     public void setAddress(int address) {
         this.address = address;
+    }
+
+    public void setAddress(String address) {
+        String[] splitAddress = address.split("\\.", 4);
+        int[] parsedAddress = Stream.of(splitAddress).mapToInt(x -> {
+            try {
+                return Integer.parseInt(x);
+            } catch (Exception e) {
+                return 0;
+            }
+        }).toArray();
+        this.address = (parsedAddress[0] << 24) + (parsedAddress[1] << 16) + (parsedAddress[2] << 8) + parsedAddress[3];
     }
 
     public void setPrefix(int prefix) {
---
+++ src/IPv4Exec.java
@@ -0,0 +1,72 @@
+import old.Message;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+public class IPv4Exec {
+    static String inputData;
+    public static void main(String[] args) {
+        String inputIPv4Address;
+        int inputPrefixLength;
+
+        if(args.length != 0){
+            // address receive from argument
+            inputData = args[0];
+        } else {
+            // address receive from stdin
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            if (dialogue(bufferedReader)) {
+                return;
+            }
+        }
+
+        String[] splitPrefix = inputData.split("/", 2);
+        inputIPv4Address = splitPrefix[0];
+        try{
+            inputPrefixLength = Integer.parseInt(splitPrefix[1]);
+        } catch (NumberFormatException e){
+            System.out.println("Error: Prefix is NaN");
+            return;
+        }
+
+        // check prefix range
+        if (inputPrefixLength < 1 || inputPrefixLength > 30) {
+            System.out.println("Error: Invalid Prefix Length");
+        }
+
+        IPv4 iPv4 = new IPv4(inputIPv4Address, inputPrefixLength);
+        System.out.println("IPv4 Address: " + buildAddress(iPv4.getAddress()));
+        System.out.println("Prefix Length: " + buildAddress(iPv4.getPrefix()));
+
+        // show subnet mask
+        System.out.println("Subnet Mask: " + buildAddress(iPv4.getNetMask()));
+
+        // snow network address
+        System.out.println("Network Address: " + buildAddress(iPv4.getNetworkAddress()));
+
+        // calculate and show broadcast address
+        System.out.println("Broadcast Address: " + buildAddress(iPv4.getBroadcastAddress()));
+
+        // show number of hosts
+        int numberOfHosts = ~iPv4.getNetMask() - 1;
+        System.out.println("Number of Hosts: " + numberOfHosts);
+    }
+
+    private static boolean dialogue(BufferedReader bufferedReader) {
+        try {
+            System.out.println("Input IPv4 Address and Prefix.");
+            System.out.println("e.g.) 133.17.0.1/24");
+            inputData = bufferedReader.readLine();
+        } catch (Exception e) {
+            System.out.println("Error");
+            return true;
+        }
+        return false;
+    }
+
+    private static String buildAddress(int address) {
+        return (address >>> 24) + "." + ((address >>> 16) & 0xff) + "." +
+                ((address >>> 8) & 0xff) + "." + (address & 0xff);
+    }
+}
